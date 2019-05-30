2bc82bfc8873196e8dbd7291b367f6c9f054ff09
MarinescuBogdan <49790748+MarinescuBogdan@users.noreply.github.com>
2019-05-30 16:58:28 +0300

    Add files via upload
    Inheritance

---
+++ Car.java
@@ -0,0 +1,20 @@
+package main.java.inheritance;
+
+public class Car extends Vehicle {
+
+    public Car(int noOfWheels) {
+        super(noOfWheels); // super() must be the first statement in the constructor;
+        int noOfWh = super.noOfWheels; // we can call super fields
+        String vehicle = super.toString(); // we can call super methods
+    }
+
+    public Car(){
+        super("Calling Super constructor with 2 parameters",
+                "and it works even if there is no default constructor specified");
+    }
+
+    @Override
+    public String maxSpeed(){
+        return "125kmph";
+    }  //maxSpeed will override the maxSpeed from Vehicle class
+}
---
+++ RaceCar.java
@@ -0,0 +1,36 @@
+package main.java.inheritance;
+
+public class RaceCar extends Car {
+
+    String superColor;
+
+    public boolean isFast() {
+        return fast;
+    }
+
+    boolean fast;
+
+    public RaceCar(int noOfWheels) {
+    }
+
+
+    public void something(){}
+
+    public String maxSpeed(){ //maxSpeed overrides the method from Car class
+        return "300kmph";
+    }
+
+    public String maxSpeed( int distance){ // maxSpeed(int distance) overloads the maxSpeed() method. It is different than overriding.
+        maxSpeed("",3);
+        return "4000";
+    }
+
+    public String maxSpeed(String x, int distance){
+        distance = 5;
+        return "4000";
+    }
+    public String maxSpeed(final int distance, String x){
+
+        return "4000";
+    }
+}
